51e8fcfdc26fae5b73b765ff499b69bf507dc48f
lmbrmb <lmbrmb@gmail.com>
2021-01-04 03:43:52 +0300

    1. UE project setup: C++ and starter assets 2. Custom classes and blueprints added

---
+++ Source/LetsGo/LetsGoGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LetsGoGameModeBase.h"
+#include "LetsGoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETSGO_API ALetsGoGameMode : public ALetsGoGameModeBase
+{
+	GENERATED_BODY()
+	
+};
